af62c9dec92b562278f00f20879f754407237386
Santiago <santi-casanova_@hotmail.com>
2022-06-09 20:30:04 -0300

    updates

--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,13 +1,29 @@
 import styles from './ProductItem.module.scss'
 import { Product } from '../product/types'
+import { useState } from 'react'
 
 interface Props {
   product: Product
 }
 
 function ProductItem({ product }: Props) {
+  const [box, setBox] = useState(false)
+
   return (
-    <div className={styles.container}></div>
+    <div className={styles.container} onMouseEnter={() => { setBox(true) }} onMouseLeave={() => { setBox(false) }}>
+      <img src={product.image} alt="productImage" className={styles.container__image} />
+      {
+        box &&
+        <div className={styles.hover}>
+          <img src="./public/world.svg" alt="world" className={styles.hover__image} />
+          <span className={styles.hover__text}>ADD TO CART</span>
+        </div>
+      }
+      <div className={styles.container__bottom}>
+        <span className={styles.bottom__title}>{product.title}</span>
+        <span className={styles.bottom__price}>${product.price}</span>
+      </div>
+    </div>
   )
 }
 
