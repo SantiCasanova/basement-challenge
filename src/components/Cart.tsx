d2ac22dc391ba6777173b9a40262c0dfa4ece1ee
Santiago <santi-casanova_@hotmail.com>
2022-06-10 20:18:44 -0300

    BIGUPDATES

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,17 +1,33 @@
 import styles from './Cart.module.scss'
 import CartItem from './CartItem'
 
-function Cart() {
+interface Props {
+  show: boolean,
+  closeModal: any  //HELP
+}
+
+function Cart({ show, closeModal }: Props) {
+  if (!show) {
+    return null
+  }
   return (
     <div className={styles.container}>
-      <button className={styles.container__button}>CLOSE</button>
-      <div className={styles.container__title}>
+      <button className={styles.container__button} onClick={closeModal}>CLOSE</button>
+      <div className={styles.container__title} onClick={e => e.stopPropagation()}>
         <span className={styles.your}>YOUR</span>
         <span className={styles.cart}>CART</span>
       </div>
       <CartItem />
+      <div className={styles.bottom}>
+        <div className={styles.bottom__total}>
+          <span>TOTAL:</span>
+          <span>$37.50</span>
+        </div>
+        <button className={styles.bottom__button}>CHECKOUT</button>
+      </div>
     </div>
   )
 }
 
 export default Cart
+
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,16 +1,19 @@
 import styles from './Nav.module.scss'
 import Cart from './Cart'
+import { useState } from 'react'
 
 function Nav() {
+  const [show, setShow] = useState(false)
+
   return (
     <div className={styles.nav}>
       <img src="./public/logo.svg" alt="logo" className={styles.nav__logo} />
       <img src="./public/hd-4k.svg" alt="hd-4k" className={styles.nav__hd4k} />
-      <button className={styles.nav__button}>
+      <button className={styles.nav__button} onClick={() => { setShow(true) }}>
         <span>CART</span>
         <span className={styles.button__item}>(0)</span>
       </button>
-      <Cart />
+      <Cart closeModal={() => { setShow(false) }} show={show} />
     </div>
   )
 }
