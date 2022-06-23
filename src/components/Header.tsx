70345e86260a53c5ff087ef9d33610a1de28ef29
Santiago <santi-casanova_@hotmail.com>
2022-06-23 20:22:08 -0300

    responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,9 @@ import styles from './Header.module.scss'
 
 function Header() {
   return (
-    <>
-      <img src="./public/header.svg" alt="header" className={styles.header} />
-    </>
+    <div className={styles.container}>
+      <img src="./public/header.svg" alt="header" className={styles.container__header} />
+    </div>
   )
 }
 
