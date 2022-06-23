cdc6da5c9cdbd0b47607e5daf412f161945d3ee9
Santiago <santi-casanova_@hotmail.com>
2022-06-23 15:29:00 -0300

    responsive

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -1,13 +1,14 @@
 import styles from './Store.module.scss'
 import ProductItem from './ProductItem'
 import { CartState } from '../context/CartContext'
+import { Product } from './interfaces/types';
 
 
 function Store() {
   const { state: { products } } = CartState();
   return (
     <div className={styles.container}>
-      {products.map(product => <ProductItem key={product.title} product={product} />)}
+      {products.map((product: Product) => <ProductItem key={product.title} product={product} />)}
     </div>
   )
 }
