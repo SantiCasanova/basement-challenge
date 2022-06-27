4e9c2856f1f583eecc8b932e6c4f8293050948da
Santiago <santi-casanova_@hotmail.com>
2022-06-27 14:33:34 -0300

    responsive

--- src/components/Separator.tsx
+++ src/components/Separator.tsx
@@ -3,11 +3,13 @@ import { motion } from "framer-motion"
 
 function Separator() {
   return (
-    <div className={styles.container}>
-      {/* animate={{ x: [-100, 100] }} transition={{ yoyo: Infinity, duration: 2, ease: 'linear' }} */}
-      <img src="./public/asterisk.svg" alt="asterisk" className={styles.container__asterisk} />
-      <motion.span className={styles.container__text}>A man can't  have enough basement. swag — A man can't have enough basement. swag</motion.span>
-      <img src="./public/asterisk2.svg" alt="asterisk" className={styles.container__asterisk2} />
+    <div className={styles.overflow}>
+      <div className={styles.container}>
+        {/* animate={{ x: [-100, 100] }} transition={{ yoyo: Infinity, duration: 2, ease: 'linear' }} */}
+        <img src="./public/asterisk.svg" alt="asterisk" className={styles.container__asterisk} />
+        <motion.span className={styles.container__text}>A man can't  have enough basement. swag — A man can't have enough basement. swag</motion.span>
+        <img src="./public/asterisk2.svg" alt="asterisk" className={styles.container__asterisk2} />
+      </div>
     </div>
   )
 }
